124380914c37dec14439f7428800f2f896adfeb0
Cesar Romero <rasec.romero46@gmail.com>
2025-05-30 18:24:41 -0600

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Error conectando a la base de datos:', err);
+    return;
+  }
+  console.log('Conectado a MySQL');
+});
+
+app.get('/', (req, res) => {
+  res.send('El tepoz es bien pero bien pendejo');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
